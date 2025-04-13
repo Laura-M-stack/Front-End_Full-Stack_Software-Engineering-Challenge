4517f4d1df08ffa9d085dac383fe0002b474ac7d
Laura Moyano <lauraymh000@hotmail.com>
2025-04-13 20:08:39 -0300

    feat(data): add static mock product list with images
    - Added a static list of 15 mock products for display in the product grid.
    - Each product includes id, name, price, originalPrice, description, and a Webpack-resolved image path.
    - Data structure is strongly typed using the 'Product' interface.
    - This list enables dynamic rendering of product cards throughout the UI.
    - Added inline comments to explain the purpose and structure of the dataset.

---
+++ src/data/products.ts
@@ -0,0 +1,128 @@
+import { Product } from '../types/Product';
+
+// Static list of products used as mock data for the product grid.
+// Each product follows the 'Product' interface structure for type safety.
+
+export const products: Product[] = [
+  
+  {
+    id: 1,
+    name: 'Baby Bear',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/baby-bear.png')
+  },
+  {
+    id: 2,
+    name: 'Baby Capy',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/baby-capy.png')
+  },
+  {
+    id: 3,
+    name: 'Baby Duck',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/baby-duck.png')
+  },
+  {
+    id: 4,
+    name: 'Chick Gold',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/Chick-Gold.png')
+  },
+  {
+    id: 5,
+    name: 'Cute Alien',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/cute-alien.png')
+  },
+  {
+    id: 6,
+    name: 'Cute Waffle',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/cute-waffle.png')
+  },
+  {
+    id: 7,
+    name: 'Talking Tom',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/talking-tom.png')
+  },
+  {
+    id: 8,
+    name: 'Tiny Duck',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/tiny-duck.png')
+  },
+  {
+    id: 9,
+    name: 'Happy Toast',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/happy-toast.png')
+  },
+  {
+    id: 10,
+    name: 'Kawaii Food Cart',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/kawaii-food-cart.png')
+  },
+  {
+    id: 11,
+    name: 'Pianosaurus',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/pianosaurus.png')
+  },
+  {
+    id: 12,
+    name: 'SharkCat Suit',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/shark-cat-suit.png')
+  },
+  {
+    id: 13,
+    name: 'Spider Kitty',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/spider-kitty.png')
+  },
+  {
+    id: 14,
+    name: 'Stitch Valentine',
+    price: 320,
+    originalPrice: 400,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/stitch-valentine.png')
+  },
+  {
+    id: 15,
+    name: 'Vendor Machine',
+    price: 450,
+    originalPrice: 522.5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: require('../assets/vendor-machine.png')
+  }
+];
